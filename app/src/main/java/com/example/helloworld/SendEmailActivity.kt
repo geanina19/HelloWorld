49956a4243f2cf1bf41d9a54120d764ff63ba1db
Geanina <geany849@gmail.com>
2023-11-22 06:35:48 +0100

    Modificado send email

--- app/src/main/java/com/example/helloworld/SendEmailActivity.kt
+++ app/src/main/java/com/example/helloworld/SendEmailActivity.kt
@@ -8,9 +8,15 @@ class SendEmailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySendEmailBinding
 
+    private var contador = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySendEmailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.enviarButton.setOnClickListener {
+            contador++
+            binding.prueba.text = contador.toString()
+        }
     }
 }
