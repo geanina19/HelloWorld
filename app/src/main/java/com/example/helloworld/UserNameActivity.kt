85255cf455ccd7a5a4dd6560a5793ef67f9a5c8f
Geanina <geany849@gmail.com>
2024-01-17 10:25:15 +0100

    ns

--- app/src/main/java/com/example/helloworld/UserNameActivity.kt
+++ app/src/main/java/com/example/helloworld/UserNameActivity.kt
@@ -1,9 +1,7 @@
 package com.example.helloworld
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.helloworld.databinding.ActivityUserNameBinding
 
 class UserNameActivity : AppCompatActivity() {
@@ -14,34 +12,34 @@ class UserNameActivity : AppCompatActivity() {
         binding = ActivityUserNameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.abrirDisplayUserNameActivityButton.setOnClickListener() {
-
-            //si se ha dejado el editText vacio saldrá un mensaje(Toast)
-            if(binding.nombreEditText.text.isEmpty()) {
-                Toast.makeText(
-                    this,
-                    "Debe introducir un nombre de usuario",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-            else {
-                //con el Intent abrimos otra actividad
-                val intent = Intent (
-                    this,
-                    DisplayUserNameActivity::class.java
-                )
-
-                /*le decimos que además de que abra la acividad display,
-                añada la info pasada en el editText
-                 */
-                intent.putExtra (
-                    DisplayUserNameActivity.Params.USERNAME.name,
-                    binding.nombreEditText.text.toString()
-                )
-
-                startActivity(intent)
-            }
-        }
+//        binding.abrirDisplayUserNameActivityButton.setOnClickListener() {
+//
+//            //si se ha dejado el editText vacio saldrá un mensaje(Toast)
+//            if(binding.nombreEditText.text.isEmpty()) {
+//                Toast.makeText(
+//                    this,
+//                    "Debe introducir un nombre de usuario",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//
+//            else {
+//                //con el Intent abrimos otra actividad
+//                val intent = Intent (
+//                    this,
+//                    DisplayUserNameActivity::class.java
+//                )
+//
+//                /*le decimos que además de que abra la acividad display,
+//                añada la info pasada en el editText
+//                 */
+//                intent.putExtra (
+//                    DisplayUserNameActivity.Params.USERNAME.name,
+//                    binding.nombreEditText.text.toString()
+//                )
+//
+//                startActivity(intent)
+//            }
+//        }
     }
 }
